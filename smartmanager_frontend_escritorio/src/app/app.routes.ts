e23a371208219ee2173fee6ecc3cacf44f83662b
Fernando Henn <102255890+FerHenn@users.noreply.github.com>
2024-10-05 18:51:00 -0300

    Paginas a usar y sus componentes.
    Se agregaron diferentes paginas principales que se rellenaran con diferentes datos del negocio.

--- smartmanager_frontend_escritorio/src/app/app.routes.ts
+++ smartmanager_frontend_escritorio/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component'; 
+import { PersonalComponent } from './personal/personal.component'; 
+import { InicioComponent} from './inicio/inicio.component'; 
+import { VentasComponent } from './ventas/ventas.component'; 
+import { StockComponent } from './stock/stock.component'; 
+import { DashboardComponent } from './dashboard/dashboard.component'; 
+import { RegistroComponent } from './registro/registro.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'personal', component: PersonalComponent },
+  { path: 'stock', component: StockComponent },
+  { path: 'ventas', component: VentasComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'registro', component: RegistroComponent },
+  // Otras rutas...
+];
+export const routing = RouterModule.forRoot(routes);
