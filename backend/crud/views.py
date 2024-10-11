b69137ae8b5e4f9bee8a006349b87142860427da
isackson99 <is.serrano@duocuc.cl>
2024-10-10 22:32:51 -0300

    Actualizacion de modelo y usuario

--- backend/crud/views.py
+++ backend/crud/views.py
@@ -1,38 +1,14 @@
 from rest_framework import viewsets
-from .serializers import UsuarioSerializer, RolSerializer, PermisoSerializer, UsuarioEmpresaSerializer, ProveedorSerializer, ClienteSerializer, ChatbotSerializer, InteraccionSerializer, CategoriaSerializer, ProductoSerializer, MetodoPagoSerializer, OrdenCompraSerializer, CarritoSerializer, PedidoSerializer, ReporteSerializer     
-from .models import Usuario, Rol, Permiso, UsuarioEmpresa, Proveedor, Cliente, Chatbot, Interaccion, Categoria, Producto, MetodoPago, OrdenCompra, Carrito, Pedido, Reporte     
-
+from .serializers import UsuarioSerializer, UsuarioSerializer, ProveedorSerializer, CategoriaSerializer, ProductoSerializer, MetodoPagoSerializer, OrdenCompraSerializer, ReporteSerializer     
+from .models import Usuario, Usuario, Proveedor, Categoria, Producto, MetodoPago, OrdenCompra, Reporte 
+    
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset=Usuario.objects.all()
     serializer_class=UsuarioSerializer 
     
-class RolViewSet(viewsets.ModelViewSet):
-    queryset=Rol.objects.all()
-    serializer_class=RolSerializer 
-    
-class PermisoViewSet(viewsets.ModelViewSet):
-    queryset=Permiso.objects.all()
-    serializer_class=PermisoSerializer 
-    
-class UsuarioEmpresaViewSet(viewsets.ModelViewSet):
-    queryset=UsuarioEmpresa.objects.all()
-    serializer_class=UsuarioEmpresaSerializer 
-    
 class ProveedorViewSet(viewsets.ModelViewSet):
     queryset=Proveedor.objects.all()
     serializer_class=ProveedorSerializer 
-    
-class ClienteViewSet(viewsets.ModelViewSet):
-    queryset=Cliente.objects.all()
-    serializer_class=ClienteSerializer 
-
-class ChatbotViewSet(viewsets.ModelViewSet):
-    queryset=Chatbot.objects.all()
-    serializer_class=ChatbotSerializer 
-    
-class InteraccionViewSet(viewsets.ModelViewSet):
-    queryset=Interaccion.objects.all()
-    serializer_class=InteraccionSerializer 
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset=Categoria.objects.all()
@@ -49,15 +25,7 @@ class MetodoPagoViewSet(viewsets.ModelViewSet):
 class OrdenCompraViewSet(viewsets.ModelViewSet):
     queryset=OrdenCompra.objects.all()
     serializer_class=OrdenCompraSerializer 
-    
-class CarritoViewSet(viewsets.ModelViewSet):
-    queryset=Carrito.objects.all()
-    serializer_class=CarritoSerializer 
-    
-class PedidoViewSet(viewsets.ModelViewSet):
-    queryset=Pedido.objects.all()
-    serializer_class=PedidoSerializer 
-    
+      
 class ReporteViewSet(viewsets.ModelViewSet):
     queryset=Reporte.objects.all()
     serializer_class=ReporteSerializer 
