28405dad076ccc6270d572c877c32c3c1977c30e
isackson99 <is.serrano@duocuc.cl>
2024-11-19 09:16:02 -0300

    Cambio de iconos

--- smartmanager_frontend_escritorio/src/app/components/navbar/navbar.component.ts
+++ smartmanager_frontend_escritorio/src/app/components/navbar/navbar.component.ts
@@ -60,15 +60,15 @@ export class NavbarComponent implements OnInit {
         this.menuItems = [
           { label: 'Inicio', icon: 'pi pi-home', routerLink: ['/inicio'] },
           { label: 'Productos', icon: 'pi pi-cog', routerLink: ['/productos'] },
-          { label: 'Carrito', icon: 'pi pi-cog', routerLink: ['/carrito'] },
+          { label: 'Carrito', icon: 'pi pi-shopping-cart', routerLink: ['/carrito'] },
         ];
 
         // Agregar opciones de administración si el usuario es administrador
         if (perfil.role === 'Administrador') {
           this.menuItems.push(
-            { label: 'Usuario', icon: 'pi pi-calendar', routerLink: ['/usuario'] },
+            { label: 'Usuario', icon: 'pi pi-user', routerLink: ['/usuario'] },
             { label: 'Ventas', icon: 'pi pi-cog', routerLink: ['/ventas'] },
-            { label: 'Registro', icon: 'pi pi-cog', routerLink: ['/registro'] },
+            { label: 'Registro', icon: 'pi pi-user', routerLink: ['/registro'] },
             { label: 'Dashboard', icon: 'pi pi-cog', routerLink: ['/dashboard'] },
             { label: 'Recuperar Contraseña', icon: 'pi pi-cog', routerLink: ['/recuperar-contrasena'] }
           );
