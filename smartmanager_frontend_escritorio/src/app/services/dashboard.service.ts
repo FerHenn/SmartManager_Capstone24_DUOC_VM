75e3c90a04c34ef3cdf01b466d47d63d822b2267
isackson99 <is.serrano@duocuc.cl>
2024-11-19 09:16:34 -0300

    se crea el servicio de dashboard

---
+++ smartmanager_frontend_escritorio/src/app/services/dashboard.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { VentasDiarias, VentasMensuales, ResumenInventario } from '../interfaces/dashboard.interface'; // Importar las interfaces
+import { environment } from '../../environments/environment'; // Importar las URLs desde el environment
+
+@Injectable({
+  providedIn: 'root', // Hace que el servicio esté disponible globalmente
+})
+export class DashboardService {
+  private apiBaseUrl = environment.apiUrl; // Se usa el baseUrl según el entorno (local o nube)
+
+  constructor(private http: HttpClient) {}
+
+  getResumenInventario(): Observable<ResumenInventario> {
+    return this.http.get<ResumenInventario>(`${this.apiBaseUrl}resumen-inventario-diario/`);
+  }
+
+  getVentasDiarias(): Observable<VentasDiarias> {
+    return this.http.get<VentasDiarias>(`${this.apiBaseUrl}reporte-ventas-diario/`);
+  }
+
+  getVentasMensuales(): Observable<VentasMensuales> {
+    return this.http.get<VentasMensuales>(`${this.apiBaseUrl}reporte-ventas-mensual/`);
+  }
+}
