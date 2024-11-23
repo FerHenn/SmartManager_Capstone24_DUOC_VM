80045bc42285dd1c1ad1936b9dbe479fbc877cb8
isackson99 <is.serrano@duocuc.cl>
2024-11-23 17:29:32 -0300

    Se accederan a las imagenes desde la raiz

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,19 +1,5 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls  # Incluye documentación de API
@@ -25,3 +11,5 @@ urlpatterns = [
     path('api/', include('crud.urls')),  # Incluye las URLs de la app CRUD para la API
     path('docs/', include_docs_urls(title='Crud documentation')),  # Documentación de la API con coreapi
 ]
+if settings.DEBUG:  # Serve media files only in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
