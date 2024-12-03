fee111c9832a2a10d7e2123cdf57d40bd395c2ba
isackson99 <is.serrano@duocuc.cl>
2024-12-03 15:44:00 -0300

    Se cambia nombre, de agrega inco y splash y se corrige el tema a claro

--- smartmanager_frontend_ionic/capacitor.config.ts
+++ smartmanager_frontend_ionic/capacitor.config.ts
@@ -2,8 +2,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'smartmanager_frontend_ionic',
-  webDir: 'www'
+  appName: 'Smart Manager',
+  webDir: 'www',
+  bundledWebRuntime: false,
 };
 
 export default config;
